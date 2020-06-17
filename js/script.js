2ffcd8848d5864af87e763d60766388eed588dd0
sophiamarinelli <maris170@newschool.edu>
2020-06-17 08:52:22 -0600

    fixed CSS menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 $(function() {
-    $newdiv = $(`<div class="menuSpacer"><a href="">PROJECT 1</a><br><a href="">PROJECT 2</a></div>`)
-    $("#projectsWing").hide()
-    $('.menu ul li').hover(function() {
-        $(this).children('ul').stop(true, false, true).slideToggle(300);
-    });
-
-
+    $(".footer").fitText(1);
 });
