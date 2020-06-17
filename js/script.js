8f2117106d8b8a7ff393ea1328ff8ebf73571c7c
sophiamarinelli <maris170@newschool.edu>
2020-06-17 08:34:54 -0600

    menu using CSS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(function() {
+    $newdiv = $(`<div class="menuSpacer"><a href="">PROJECT 1</a><br><a href="">PROJECT 2</a></div>`)
     $("#projectsWing").hide()
     $('.menu ul li').hover(function() {
         $(this).children('ul').stop(true, false, true).slideToggle(300);
     });
 
-    $('#stretch').fitText(1, { minFontSize: '48px', maxFontSize: '20000px' })
+
 });
