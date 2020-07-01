ed3feb0eaced7a5103f55b471e9d38ebe2b4c4a2
sophiamarinelli <maris170@newschool.edu>
2020-07-01 12:57:13 -0600

    images, svg footer, positioning

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+console.log('hello')
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
+
+
 $(function() {
-    $(".footer").fitText(1);
+
+    $("video").prop('muted', true);
+
+    $(".mute-video").click(function() {
+        if ($("video").prop('muted')) {
+            $("video").prop('muted', false);
+            $(this).addClass('unmute-video');
+
+        } else {
+            $("video").prop('muted', true);
+            $(this).removeClass('unmute-video');
+        }
+        console.log($("video").prop('muted'))
+    });
 });
